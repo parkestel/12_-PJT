607b238db33ce863900b350d0a38e1f61dbaa143
PARK Jaeyoung <estel2005@naver.com>
2024-11-10 21:41:54 +0900

    FEAT:modify MovieLanguage field name and field type(tmdb_id to iso_code and int to text field)

--- database_creates/models.py
+++ database_creates/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class MovieLanguage:
-    tmdb_id = models.IntegerField()
+    iso_code = models.TextField()
     name = models.TextField()
 
 
