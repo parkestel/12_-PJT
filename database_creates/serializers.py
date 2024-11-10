717512f8b31d43c3fecbb6579ffd9ab115f0f0c8
PARK Jaeyoung <estel2005@naver.com>
2024-11-10 21:34:20 +0900

    FEAT:create MovieLanguageSerializier

--- database_creates/serializers.py
+++ database_creates/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from  .models import Genre
+from .models import Genre, MovieLanguage
+
 
 class GenreSerialzier(serializers.ModelSerializer):
     class Meta:
         model = Genre
+        fields = "__all__"
+
+
+class MovieLanguageSerialzier(serializers.ModelSerializer):
+    class Meta:
+        model = MovieLanguage
         fields = "__all__"
